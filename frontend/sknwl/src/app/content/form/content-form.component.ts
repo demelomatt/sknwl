bb2d72b4aa8619b386630372c82e082c165162e3
Matheus Melo <mattmelo99@gmail.com>
2023-11-27 19:52:06 -0300

    currency

--- frontend/sknwl/src/app/content/form/content-form.component.ts
+++ frontend/sknwl/src/app/content/form/content-form.component.ts
@@ -112,7 +112,7 @@ export class ContentFormComponent implements OnInit{
         language: this.contentForm.value.language,
         studyField: Object.hasOwn(this.selectedField , 'id') ? this.selectedField : {name: this.contentForm.value.studyField},
         durationMinutes: this.contentForm.value.durationMinutes,
-        price: {amount: this.contentForm.value.price, currency: {id: 19, code: 'BRL'}},
+        price: {amount: this.contentForm.value.price, currency: {id: 18, code: 'BRL'}},
         publisher: {
           id: 1,
           name: 'System User'
